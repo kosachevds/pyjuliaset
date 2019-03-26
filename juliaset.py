32e5077f777329c6372d5eb2d8038f09bd9e4115
Daniil Kosachev <kos-dan@mail.ru>
2019-03-26 19:42:00 +0300

    remade with function _count_iterations

--- juliaset.py
+++ juliaset.py
@@ -1,8 +1,8 @@
 import math
-from matplotlib import pyplot as pp
+from PIL import Image
 
 
-def plot(c_value, iteration_count, height=1000, width=1000):
+def plot(c_value, max_iteration_count, height=500, width=500):
     r_value = _compute_r(c_value)
     x_min = -r_value
     y_min = -r_value
@@ -11,36 +11,27 @@ def plot(c_value, iteration_count, height=1000, width=1000):
     x_step = abs(x_min - x_max) / width
     y_step = abs(y_min - y_max) / height
 
-    xy_idx = {}
-    max_idx = 0
+    bitmap = Image.new("RGB", (width, height), "white")
+    pixels = bitmap.load()
     for i in range(width):
-        xy_idx[i] = {}
         x_i = x_min + i * x_step
         for j in range(height):
             y_j = y_min + j * y_step
             z_0 = complex(x_i, y_j)
-            z_values = _compute_points(z_0, c_value, iteration_count, r_value)
-            idx = len(z_values) - 1
-            max_idx = max(max_idx, idx)
-            xy_idx[i][j] = idx
+            count = _count_iterations(z_0, c_value, max_iteration_count, r_value)
+            pixels[i, j] = _get_color(count)
+    bitmap.show()
 
-    for i in range(width):
-        x_i = x_min + i * x_step
-        for j in range(height):
-            y_j = y_min + j * y_step
-            z_ij = complex(x_i, y_j)
-            if abs(z_ij) / r_value > 1:
-                pp.plot(x_i, y_j)
-    pp.show()
 
-def _compute_points(z_0, c_value, iteration_count, r_value):
-    result = [z_0]
+def _count_iterations(z_0, c_value, iteration_count, r_value):
+    count = 0
+    current_value = z_0
     for _ in range(iteration_count):
-        previous = result[-1]
-        if abs(previous) > r_value > 0:
+        if abs(current_value) > r_value > 0:
             break
-        result.append(previous * previous + c_value)
-    return result
+        current_value = current_value ** 2 + c_value
+        count += 1
+    return count
 
 
 def _compute_r(c_value):
